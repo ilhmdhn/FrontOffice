85b288b4e0feee57ebe061d38b5593d7f166dc52
Ilham Dohaan <doha39anilham@gmail.com>
2022-08-08 11:14:20 +0700

    update ui fnb

--- app/src/main/java/com/ihp/frontoffice/view/fragment/notification/NotificationAdapter.kt
+++ app/src/main/java/com/ihp/frontoffice/view/fragment/notification/NotificationAdapter.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.ihp.frontoffice.R
 import com.ihp.frontoffice.data.remote.respons.DataRoomCall
